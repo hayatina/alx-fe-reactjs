fe045e82112dc0de2de12d328b8710e5d1960128
Hayat <hayatmusa3229@gmail.com>
2025-02-23 22:28:17 +0300

    adding duplicate file of alx-react-app with inline css styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{backgroundColor:"rebeccapurple", padding:'10px', margin:"10px"}}>
+      <p style={{color:"gray"}}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: "gray", border:'1px solid black' }}>
+      <p style={{ color: "black" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
