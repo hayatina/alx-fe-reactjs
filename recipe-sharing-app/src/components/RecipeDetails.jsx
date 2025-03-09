0d1b81bca5004f885defc770bf6dfa3a7fe5f523
Hayat <hayatmusa3229@gmail.com>
2025-03-09 22:26:26 +0300

    add edit  , delete and detail for  form using Zustand

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -9,6 +9,10 @@ const RecipeDetails = ({ recipeId }) => {
     state.recipes.find((recipe) => recipe.id === recipeId)
   );
 
+  if (!recipe) {
+    return <p>Recipe not found.</p>;
+  }
+
   return (
     <div>
       <h1>{recipe.title}</h1>
