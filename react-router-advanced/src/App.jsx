c644621c1985f2d50e3638c15a4407cab454198c
Hayat <hayatmusa3229@gmail.com>
2025-03-16 22:04:24 +0300

    intiate react quer route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,20 +3,26 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import ProfileDetails from "./pages/ProfileDetails";
+import ProfileSettings from "./pages/ProfileSettings";
+import BlogPost from "./pages/BlogPost"; // Import BlogPost component
 import NotFound from "./pages/NotFound";
-import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Home Route */}
         <Route path="/" element={<Home />} />
+        {/* Protected Route for Profile */}
         <Route
           path="/profile/*"
           element={<ProtectedRoute element={<Profile />} />}
         />
-        <Route path="/post/:postId" element={<BlogPost />} />
+        {/* Dynamic Blog Post Route */}
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic Route */}
+        {/* 404 Not Found Route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
